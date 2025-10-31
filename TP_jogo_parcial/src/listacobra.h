3a84e7b96647a5aadb768916d3b0a35121c2b50f
AmandaAlbuquerque07 <amandacarolalb@gmail.com>
2025-10-31 12:29:30 -0300

    Implementação da lista encadeada e adição de  novas funções
    A lista encadeada para o crescimento da "cobrinha" foi implementada e foram adicionadas as funções para criar uma lista vazia, iniciar a cobrinha com uma cabeça e uma cauda e colisão com as bordas

--- TP_jogo_parcial/src/listacobra.h
+++ TP_jogo_parcial/src/listacobra.h
@@ -7,43 +7,59 @@
 #define STD_SIZE_Y 40
 #define TEMPO 0.2
 #define COOLDOWN 0.2
-#define SNAKE_COLOR YELLOW
-#define FOOD_COLOR BLUE
+#define SNAKE_COLOR GREEN
+#define FOOD_COLOR RED
 
 typedef struct Bordas{
     Rectangle pos;
 }Bordas;
-
-typedef struct Body{
-    Rectangle pos;
-    Color color;
-    int direcao;
-}Body;
 
 typedef struct Food{
     Rectangle pos;
     Color color;
 }Food;
 
-typedef struct Jogo{
+//Início da Implementação da lista:
+
+typedef struct Body{
+    Rectangle pos;
+    Color color;
+    int direcao;
+}Body; //TipoItem
+
+typedef struct CelulaSnake * SnakeApontador;
+
+typedef struct CelulaSnake {
     Body body;
+    SnakeApontador Prox;
+} CelulaSnake; //TipoCelula
+
+typedef struct {
+    SnakeApontador Cabeca, Cauda;
+    int Comprimento;
+    int dirX, dirY;
+}ListaSnake;
+
+typedef struct Jogo{
+    ListaSnake snake;
     Food food;
     Bordas bordas[4];
     double tempo;
     double cooldown;
 }Jogo;
 
-
-void IniciaBody(Jogo *j);
+void VSvazia(ListaSnake *snake);
+void IniciaSnake(Jogo *j);
+void AumentaSnake(Jogo *j);
 void IniciaBordas(Jogo *j);
 void IniciaFood(Jogo *j);
 void IniciaJogo(Jogo *j);
-void DesenhaBody(Jogo *j); 
+void DesenhaSnake(Jogo *j);
 void DesenhaFood(Jogo *j); //Desenha uma comida em uma posição aleatória
 void DesenhaBordas(Jogo *j);
 void DesenhaJogo(Jogo *j);
 void AtualizaDirecao(Jogo *j); //Atualiza a direção da cobrinha
-void AtualizaPosBody(Jogo *j); //Atualiza a posição da cobrinha
+void AtualizaPosSnake(Jogo *j); //Atualiza a posição da cobrinha
 void AtualizaRodada(Jogo *j);
 int ColisaoFood(Jogo *j);
 int ColisaoBordas(Jogo *j);
