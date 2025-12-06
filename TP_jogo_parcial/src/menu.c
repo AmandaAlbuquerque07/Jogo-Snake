2de3b5ba69eb78cdadaf350d209efaa34fc50f56
GiovanaMenezesL <menezesfaculdade@gmail.com>
2025-12-06 20:29:07 -0300

    nova tela

--- TP_jogo_parcial/src/menu.c
+++ TP_jogo_parcial/src/menu.c
@@ -1,80 +1,92 @@
-#include "raylib.h"
-#include <stdio.h>
-#include <string.h>
+#ifndef COBRA_H
+#define COBRA_H
 
-#include "menu.h"
-#include "ranking.h"
+#define LARGURA 660
+#define ALTURA 660
+#define STD_SIZE_X 40
+#define STD_SIZE_Y 40
+#define TEMPO 0.16
+#define COOLDOWN 0.2
+#define SNAKE_COLOR VIOLET
+#define FOOD_COLOR RED
 
-#define MAX_TAM 50
+typedef struct Bordas{
+Rectangle pos;
+}Bordas;
 
-void desenhaMenuPrincipal() {
-    ClearBackground(SKYBLUE);
-    DrawText("MENU PRINCIPAL", 165, 200, 40, BLACK);
-    DrawText("1 - Ranking", 225, 300, 30, RED);
-    DrawText("2 - Jogar", 225, 350, 30, VIOLET);
-    DrawText("ESC - Sair", 225, 400, 30, DARKGRAY);
-}
+typedef struct Food{
+Rectangle pos;
+Color color;
+}Food;
 
+typedef struct Barreiras{
+Rectangle pos;
+Color color;
+int velocidade;
+int inicia;
+}Barreiras;
 
-void desenhaTelaNome(char *nomeJogador, int *tamanho) {
-    ClearBackground(SKYBLUE);
-    //desenha na tela o "digite seu nomme" e vai mostrando o que for digitado na string nomeJogador[MAX_TAM];
+typedef struct Texturas{
+    Texture2D Cabeca, Corpo, Rabo;
+    Texture2D pedras, pedras1, pedras2;
+    Texture2D tubaraoD ,tubaraoE;
+    Texture2D Food;
+}Texturas;
 
-    DrawText("Digite seu nome:", 215, 100, 30, BLACK);
-    DrawText(nomeJogador, 250, 150, 30, VIOLET);
+//Início da Implementação da lista:
 
-    //Isso aqui é o loop de coleta das letras a partir da pressão nas teclas:
-    int tecla = GetCharPressed();
-    while (tecla > 0) {
-        if ((tecla >= 32) && (tecla <= 125) && (*tamanho < MAX_TAM-1)) {
-            //porque eu quero só letras e porque tem que ter espaço pro \0 ^ ;
-            nomeJogador[*tamanho] = (char)tecla;
-            (*tamanho)++;
-            //vou andando na string ^ ;
-            nomeJogador[*tamanho] = '\0';
-        }
-        tecla = GetCharPressed();
-    }
+typedef struct Body{
+Rectangle pos;
+Texture2D color;
+int direcao;
+}Body; //TipoItem
 
-    //isso aqui permite que a pessoa apague também:
-    if (IsKeyPressed(KEY_BACKSPACE) && *tamanho > 0) {
-        (*tamanho)--;
-        nomeJogador[*tamanho] = '\0';
-    }
+typedef struct CelulaSnake * SnakeApontador;
 
-    DrawText("Pressione ENTER para começar", 175, 400, 20, GRAY);
-}
+typedef struct CelulaSnake {
+Body body;
+SnakeApontador Prox;
+} CelulaSnake; //TipoCelula
 
+typedef struct {
+SnakeApontador Cabeca, Cauda;
+int Comprimento;
+}ListaSnake;
 
+typedef struct Jogo{
+ListaSnake snake;
+Food food;
+Bordas bordas[4];
+Barreiras barreiras[10];
+Texturas tex;
+double tempo;
+double cooldown;
+}Jogo;
 
+void VSvazia(ListaSnake *snake);
+void CarregaTexturas(Jogo*j);
+void IniciaSnake(Jogo *j);
+void AumentaSnake(Jogo *j);
+void IniciaBordas(Jogo *j);
+void IniciaBarreiras1(Jogo *j);
+void IniciaFood(Jogo *j);
+void IniciaJogo(Jogo *j);
+void DesenhaSnake(Jogo *j);
+void DesenhaFood(Jogo *j); //Desenha uma comida em uma posição aleatória
+void DesenhaBordas(Jogo *j);
+void DesenhaBarreiras1(Jogo *j);
+void DesenhaBarreiras3(Jogo *j);
+void DesenhaJogo(Jogo *j);
+void AtualizaPosSnake(Jogo *j); //Atualiza a posição da cobrinha
+void AtualizaBarreiras3(Jogo *j);
+void AtualizaRodada(Jogo *j);
+void AtualizaDirecao(Jogo *j);
+int ColisaoFood(Jogo *j);
+void ColisaoBordas(Jogo *j);
+int ColisaoBarreiras1(Jogo *j);
+int ColisaoBarreiras3(Jogo* j);
+int ColisaoSnake(Jogo *j);
+void FreeLista(ListaSnake *Snake);
+void LiberaTexturas(Jogo *j);
 
-void desenhaTelaRanking() {
-    ClearBackground(SKYBLUE);
-    DrawText("RANKING", 250, 50, 40, BLACK);
-
-    //abre pra ler:
-    FILE *rk = fopen("ranking.txt", "r");
-
-    //vê se existe o ranking:
-    if (rk != NULL) {
-        int pontos;
-        char nome[50];
-        int y = 120; //esse y detremina a posição inicial das linhas do ranking em pixels no eixo vertical
-        int posicao=1; //coloca enumeração no ranking!
-
-        while (fscanf(rk, "%d %s", &pontos, nome) == 2) { //enquanto ler o nome e os pontos com sucesso,
-            char linha[120]; //esse armazena a junção dos pontos e do nome em uma linha só, pro DrawText ler, assim como o PontoNaTela, da main, que formata o ponto pro DrawText também;
-            sprintf(linha, "%dº - %s : %d pts", posicao, nome, pontos); //combinando os dois E A POSIÇÃO!;
-            DrawText(linha, 225, y, 20, RED); //texto, x, y, tam fonte, cor 
-            y += 20; //pula 20 pixels verticais pra próxima linha ficar
-            posicao++;
-        }
-        fclose(rk);
-    } else {
-        DrawText("Ranking vazio!", 250, 150, 30, RED);
-    }
-
-    DrawText("Pressione ENTER para voltar", 200, 550, 20, GRAY);
-}
-
-
+#endif
