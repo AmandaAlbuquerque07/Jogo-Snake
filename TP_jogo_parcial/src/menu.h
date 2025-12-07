90542691e4c7076b06eabf28ff5894013845cbb1
GiovanaMenezesL <menezesfaculdade@gmail.com>
2025-12-07 15:29:02 -0300

    tela inicio

--- TP_jogo_parcial/src/menu.h
+++ TP_jogo_parcial/src/menu.h
@@ -1,9 +1,12 @@
 #ifndef MENU_H
 #define MENU_H
 
+#include "listacobra.h"
+
 //enum: é uma variável que armazena 1 inteiro qualquer, que é diferente dos outros enum: marca estado;
 
 typedef enum { 
+    INTRO,
     MENU,
     NOME,
     TELAS,
@@ -12,6 +15,7 @@ typedef enum {
 } Estado;
 
 // Funções do menu
+void desenhaIntro(Jogo*j);
 void desenhaMenuPrincipal(Jogo* j);
 void desenhaTelaTelas(Jogo* j);
 void desenhaTelaNome(Jogo* j, char *nomeJogador, int *tamanho);
